a6439018dae47c6e5af749d8e57faad61dea3eb6
HRF05 <110261598+HRF05@users.noreply.github.com>
2022-08-09 23:37:54 -0400

    Update ccc00s1.cpp

--- ccc00s1.cpp
+++ ccc00s1.cpp
@@ -1,52 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
 	int q, a, b, c, tp = 0;
-	cin >> q;
-	cin >> a;
-	cin >> b;
-	cin >> c;
-	for(int i = 0; i < q; )
-	{
+	cin>>q>>a>>b>>c;
+	for(int i = 0; i < q; ){
 		a = a + 1;
 		tp = tp + 1;
 		i++;
-		if(i >= q)
-		{
+		if(i >= q){
 			cout << "Martha plays " << tp << " times before going broke." << endl;
 			return 0;
 		}
 		b = b + 1;
 		tp = tp + 1;
 		i++;
-		if(i >= q)
-		{
+		if(i >= q){
 			cout << "Martha plays " << tp << " times before going broke." << endl;
 			return 0;
 		}
 		c = c + 1;
 		tp = tp + 1;
 		i++;
-		if(i >= q)
-		{
+		if(i >= q){
 			cout << "Martha plays " << tp << " times before going broke." << endl;
 			return 0;
 		}
 		
-		if(a == 35)
-		{
+		if(a == 35){
 			i = i - 30;
 			a = 0;
 		}
-		if(b == 100)
-		{
+		if(b == 100){
 			i = i - 60;
 			b = 0;
 		}
-		if(c == 10)
-		{
+		if(c == 10){
 			i = i - 9;
 			c = 0;
 		}
