1a0879dfaa8378db2277e92b8065f5409b469288
HRF05 <110261598+HRF05@users.noreply.github.com>
2024-04-28 23:49:31 -0400

    .

--- dmopc22c5p1.cpp
+++ dmopc22c5p1.cpp
@@ -1,9 +1,12 @@
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 typedef long long ll;
 typedef long long unsigned llu;
+#define int ll
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 typedef vector<ll> vl;
@@ -16,17 +19,17 @@ struct tri {int first, second, t;bool operator<(const tri& T){return first < T.f
 #define pb push_back
 #define pf push_front
 #define mp make_pair
-#define INF 2e18
 #define su(x) do{while((x=getchar())<48); for(x-=48; 48<=(_=getchar()); x=(x<<3)+(x<<1)+_-48);}while(0)
 #define si(x) do{while((x=getchar())<45); _sign=x==45; if(_sign) while((x=getchar())<48); for(x-=48; 48<=(_=getchar()); x=(x<<3)+(x<<1)+_-48); x=_sign?-x:x;}while(0)
 #define sc(x) do{while((x=getchar())<33);}while(0)
 char _; bool _sign;
-const int MAX = 2e1 + 5;
-int n, m;
-int main(){
+const int MAX = 1e6 + 5;
+int n, ans = 0, ar[MAX];
+signed main(){
     cin.sync_with_stdio(0); cin.tie(0);
-    cin>>n>>m;
-    for(int i = 0; i < m; i++){
-        
-    }
+    cin>>n;
+    ar[0] = ar[1] = 0;
+    for(int i = 2; i < n; i++) ar[i] = ar[i-1] + i-1;
+    for(int i = 1; i*2 < n; i++) ans = max(ans, ar[n-i*2]*i);
+    cout<<ans<<endl;
 }
--- tcc1p1.cpp
+++ tcc1p1.cpp
@@ -1,7 +1,9 @@
 #pragma GCC optimize("Ofast,unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 typedef long long ll;
 typedef long long unsigned llu;
 typedef pair<int,int> pii;
@@ -21,12 +23,14 @@ struct tri {int first, second, t;bool operator<(const tri& T){return first < T.f
 #define si(x) do{while((x=getchar())<45); _sign=x==45; if(_sign) while((x=getchar())<48); for(x-=48; 48<=(_=getchar()); x=(x<<3)+(x<<1)+_-48); x=_sign?-x:x;}while(0)
 #define sc(x) do{while((x=getchar())<33);}while(0)
 char _; bool _sign;
-const int MAX = 2e1 + 5;
-int n, m;
+int n, ma = 0;
+string s;
 int main(){
     cin.sync_with_stdio(0); cin.tie(0);
-    cin>>n>>m;
-    for(int i = 0; i < m; i++){
-        
+    cin>>n>>s;
+    for(int i = 0; i < n; i++){
+        if(s[i] == 'Y') ma = i+1;
     }
+    if(ma*2 > n) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
 }
